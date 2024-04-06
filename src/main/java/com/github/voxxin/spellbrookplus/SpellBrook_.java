ae68f5f196af14905b0a9a833771da352c6d7f94
Voxxin <59192970+Voxxin@users.noreply.github.com>
2024-04-06 17:47:04 -0400

    inital push

---
+++ src/main/java/com/github/voxxin/spellbrookplus/SpellBrook_.java
@@ -0,0 +1,13 @@
+package com.github.voxxin.spellbrookplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SpellBrook_ implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/github/voxxin/spellbrookplus/client/SpellBrook_Client.java
@@ -0,0 +1,13 @@
+package com.github.voxxin.spellbrookplus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpellBrook_Client implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
